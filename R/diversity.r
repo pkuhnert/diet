71231bd607e4d2d48da412f5b8c358bea78a3e8e
Petra_Kuhnert <Petra.Kuhnert@data61.csiro.au>
2018-10-06 23:36:43 +1000

    Bug Fix

--- R/diversity.r
+++ R/diversity.r
@@ -56,6 +56,7 @@ diversity <- function(object, newdata = NULL, too.far = 0.1, LatID, LonID,
 #' @rdname diversity
 #' @import  spaMM
 #' @importFrom "latticeExtra" "layer"
+#' @importFrom "rasterVis" "levelplot"
 #' @export
 diversity.dpart <- function(object, newdata = NULL, too.far = 0.1, LatID, LonID,
                             projection = "+proj=longlat +datum=WGS84", maxpixels = 1e5,
